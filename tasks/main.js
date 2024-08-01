3fc2b8bd40a3cba761d67a8b5fe5c2aedb085cc8
yvanddniyo <nyvan80076@gmail.com>
2024-08-02 00:32:20 +0200

    deleting old app to  new changes

--- tasks/main.js
+++
@@ -1,8 +0,0 @@
-// flatMap() => combines map() and flat() into one call
-const arr = [
-    { arr: [1, 2, 3] },
-    { arr: [3, 4, 5] },
-    { arr: [5, 6, 7] }
-  ];
-let result = [...new Set (arr.flatMap(el => el.arr))]
-console.log(result);
